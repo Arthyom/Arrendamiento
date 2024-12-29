79858939cbb1263263b14093042c4f24636fcbd8
arthyom <a.gonzalezgavina@ugto.mx>
2024-12-29 20:33:08 +0000

    improving deploy for back

--- backEnd.sh
+++ backEnd.sh
@@ -2,6 +2,6 @@
 git pull origin master
 dotnet build
 #loca cd /home/frodo/Documents/Repos/Arrendamiento/Arrendamientos/bin/Debug/net8.0/
-cd /home/frodo/Documents/Arrendamiento/Arrendamientos/bin/Debug/net8.0/
+#cd /home/frodo/Documents/Arrendamiento/Arrendamientos/bin/Debug/net8.0/
 #cp -R  * /var/www/html/Arrendamientos/
 #cp -R  * /var/www/html/ArrendamientoBack/
