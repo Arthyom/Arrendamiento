71946d06fa0e3829a96cd8245d1d33c5772776ef
arthyom <a.gonzalezgavina@ugto.mx>
2024-12-29 20:08:10 +0000

    Attemping to run in same dir

--- backEnd.sh
+++ backEnd.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
-cd /var/www/html/ArrendamientoGit
 git pull origin master
 dotnet build
 #cd /home/frodo/Documents/Repos/Arrendamiento/Arrendamientos/bin/Debug/net8.0/
-cd /var/www/html/ArrendamientoGit/Arrendamientos/bin/Debug/net8.0/
+#cd /home/frodo/Documents/Arrendamiento/Arrendamientos/bin/Debug/net8.0/
 #cp -R  * /var/www/html/Arrendamientos/
-cp -R  * /var/www/html/ArrendamientoBack/
+#cp -R  * /var/www/html/ArrendamientoBack/
--- frontEnd.sh
+++ frontEnd.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #cd /home/frodo/Documents/Repos/ArrendamientosFront/
-cd /var/www/html/ArrendamientosFrontoGit/
+cd /home/frodo/Documents/ArrendamientosFront
 git pull origin master
 ng build
-cd /var/www/html/ArrendamientosFrontoGit/dist/arrendamientos-front/browser/
+cd dist/arrendamientos-front/browser/
 cp -R  * /var/www/html/Arrendamiento/
 
