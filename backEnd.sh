02a43b87109a0a93404d6e86b90c7e6e5764508d
arthyom <a.gonzalezgavina@ugto.mx>
2024-12-29 20:36:28 +0000

    moving to root back path

--- backEnd.sh
+++ backEnd.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 git pull origin master
+cd /home/frodo/Documents/Arrendamiento/
 dotnet build
 #loca cd /home/frodo/Documents/Repos/Arrendamiento/Arrendamientos/bin/Debug/net8.0/
-#cd /home/frodo/Documents/Arrendamiento/Arrendamientos/bin/Debug/net8.0/
 #cp -R  * /var/www/html/Arrendamientos/
 #cp -R  * /var/www/html/ArrendamientoBack/
