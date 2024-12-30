2339df6403b63732d289b49294b48148a177dc86
arthyom <a.gonzalezgavina@ugto.mx>
2024-12-30 03:07:11 +0000

    adding restar

--- backEnd.sh
+++ backEnd.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo systemctl stop qa.arrendamientos.netcore8.service
 git pull origin master
 cd /home/frodo/Documents/Arrendamiento/
 dotnet build
 #loca cd /home/frodo/Documents/Repos/Arrendamiento/Arrendamientos/bin/Debug/net8.0/
 #cp -R  * /var/www/html/Arrendamientos/
 #cp -R  * /var/www/html/ArrendamientoBack/
-sudo systemctl start qa.arrendamientos.netcore8.service
+#sudo systemctl stop qa.arrendamientos.netcore8.service
+sudo systemctl restart qa.arrendamientos.netcore8.service
 
 
 
