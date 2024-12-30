6f398b045c719abf56126a804b52161a80d21436
arthyom <a.gonzalezgavina@ugto.mx>
2024-12-30 02:57:30 +0000

    sdfas

--- backEnd.sh
+++ backEnd.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-systemctl stop qa.arrendamientos.netcore8.service
+sudo systemctl stop qa.arrendamientos.netcore8.service
 git pull origin master
 cd /home/frodo/Documents/Arrendamiento/
 dotnet build
 #loca cd /home/frodo/Documents/Repos/Arrendamiento/Arrendamientos/bin/Debug/net8.0/
 #cp -R  * /var/www/html/Arrendamientos/
 #cp -R  * /var/www/html/ArrendamientoBack/
-systemctl start qa.arrendamientos.netcore8.service
+sudo systemctl start qa.arrendamientos.netcore8.service
+
+
+
